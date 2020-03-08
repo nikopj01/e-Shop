589e22f3136bf5fe9b13be0990ab57ab01cdc82b
Niko Putu Jaye <nikopj01@gmail.com>
2020-03-08 20:23:43 +0800

    Remove BaseEntity, Add "Test" Controller, Update Boostrap 4

---
+++ eShop.UI/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using eShop.Core.Contracts;
+using eShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eShop.UI.Controllers
+{
+    public class TestController : Controller
+    {
+        private IRepository<Product> _contextProduct;
+
+        public TestController(IRepository<Product> product)
+        {
+            _contextProduct = product;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_contextProduct.Collection().ToList());
+        }
+    }
+}
